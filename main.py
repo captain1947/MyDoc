9ca4bce20cedb64efebab53e7caaed4df4624103
captain1947 <sayantandebin@gmail.com>
2022-01-03 01:41:15 +0530

    CHanged the main implementation of the doc.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import PySimpleGUI as sg
-import pkg_resources
+import pkgutil
 import importlib
+import sys
+import io
 import pydoc
-
 
 class App:
 	def __init__(self):
@@ -18,7 +19,7 @@ class App:
 		self.input_box=sg.Input(key='INP',size=(35,None))
 		self.dropdown=sg.DropDown(self.attributes,default_value='Available methods',key='DDN',size=(42,None))
 		self.button=sg.Button('Search',key='SRC')
-		self.checkbox=sg.Checkbox('Details Documentation',key='CBX')
+		self.checkbox=sg.Checkbox('Detailed Documentation',key='CBX')
 		self.text_box=sg.Multiline(self.data,key='TB',disabled=True,size=(60,20))
 		self.layout=[
 		[self.input_box,self.button],
@@ -41,30 +42,35 @@ class App:
 
 class Doc:
 	def __init__(self):
-		self.installed_packages = pkg_resources.working_set
-		self.installed_packages_list = []
-		for i in self.installed_packages:
-			self.installed_packages_list.append(i.key)
+		pass
+
 
 	def get_method(self,arg,is_detailed):
 		if is_detailed:
 			method_list=arg.split('.')
-			if method_list[0] in self.installed_packages_list:
+			try:
 				module=importlib.import_module(method_list[0])
 				data,attributes=self.get_doc(module,method_list,1)
-			else:
+			except:
 				module=importlib.import_module('builtins')
 				data,attributes=self.get_doc(module,method_list,0)
 			return data,attributes
 		else:
-			data=pydoc.render_doc(str, "Help on %s")
+			f=io.StringIO()
+			pydoc.doc(arg, output=f)
+			f.seek(0)
+			data=f.read()
 			return data,tuple()
 
 
 	def get_doc(self,module,module_list,n):
 		if n>=len(module_list):
 			try:
-				return module.__doc__,sorted(tuple(module.__dict__.keys(),))
+				data,attributes=module.__doc__,sorted(tuple(module.__dict__.keys(),))
+				if data:
+					return data,attributes
+				else:
+					return 'No documentation found!',attributes
 			except AttributeError as error:
 				print(error)
 				return module.__doc__,tuple()
